3ec617557b6a256470f42054a0df5d3c1b050158
Ales Rechtorik <foxxcz@gmail.com>
2023-01-10 16:45:23 +0100

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'grantex';
+  servicesPopOpen = false;
+  contactDialogOpen = false;
+
+  private _servicesPopTimer: any = undefined;
+
+  openServicesPop() {
+    clearTimeout(this._servicesPopTimer);
+
+    if (this.servicesPopOpen === false) {
+      this.servicesPopOpen = true;
+      this._servicesPopTimer = undefined;
+    }
+  }
+
+  closeServicesPop() {
+    console.log('leave', this.servicesPopOpen, this._servicesPopTimer);
+    if (this.servicesPopOpen) {
+      this._servicesPopTimer = setTimeout(() => {
+        this.servicesPopOpen = false;
+      }, 200);
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [ AppComponent ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
